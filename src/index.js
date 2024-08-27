6633a0e82abfbc88a8430a5344b57c013f58ad6d
alicedev94 <alicedev94@gmail.com>
2024-08-27 16:44:12 -0400

    https config

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-const express = require('express');
-const cors = require('express');
+const express = require("express");
+const https = require("https");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
+const port2 = 443;
+
+const fs = require("fs");
+var options = {
+  key: fs.readFileSync("./ssl/code.key"),
+  cert: fs.readFileSync("./ssl/code.crt"),
+};
 
 app.use(cors());
 
-app.get('/', (req, res)=> {
-    res.send('Hello this is node..!');
-})
-
-app.listen(port, ()=> {
-    console.log('running por in: ' + port);
+app.get("/", (req, res) => {
+  res.send("Hello this is node..!");
 });
 
-// add https too
+app.listen(port, () => {
+  console.log("running http in: " + port);
+});
+
+https.createServer(options, app).listen(port2, () => { console.log("running https in: " + port2);});
